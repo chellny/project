b51939b9db9d65d6cbe3111bd56b7b5781f11287
chellny <cchheellyy@163.com>
2024-05-09 13:01:56 +0800

    ghhh6

--- src/main/java/com/itheima/reggie/controller/chehhehhe.java
+++
@@ -1,9 +0,0 @@
-package com.itheima.reggie.controller;
-
-/**
- * @Author chelly
- * @Date:2024/05/09 12:05
- * @Discription:
- */
-public class chehhehhe {
-}
--- src/main/java/com/itheima/reggie/controller/userbad2.java
+++
@@ -1,9 +0,0 @@
-package com.itheima.reggie.controller;
-
-/**
- * @Author chelly
- * @Date:2024/05/09 11:41
- * @Discription:
- */
-public class userbad2 {
-}
